71434b2c0c6fb92b10e9758f15bd259676e69716
Haamed Lazemi <lhaamed.business@gmail.com>
2024-01-30 01:05:49 +0330

    directory capicatization changed. and because of that some functions have changed respectly for second time

---
+++ src/classes/InitialBundle.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace BladeBundler\classes;
+
+use BladeBundler\classes\linkBundle\linkBundle;
+
+class InitialBundle  {
+
+    public string $title = 'Bundle Title';
+    public LinkBundle $links;
+
+    public function __construct(?string $title = null){
+        if (!is_null($title)) $this->title = $title;
+        $this->links = new linkBundle('btn-sm');
+
+        $this->links->prependLink('بازگشت', 'javascript:history.back()', 'chevron-left');
+    }
+
+    public function setTitle(?string $title = null): static
+    {
+        if (!is_null($title)) $this->title = $title;
+        return $this;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+}
---
+++ src/services/BladeBundlerService.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace BladeBundler\services;
+
+class BladeBundlerService
+{
+    public static function doSomething($data)
+    {
+        return $data;
+    }
+}
